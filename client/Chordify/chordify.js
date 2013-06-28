42f5bd676265e2ebf58f61dc6824a54eb078687f
Phact <johncoltrane11@gmail.com>
2013-06-28 00:10:13 -0400

    Added autoscroll in the frontend. It's based on the size of the page and the track length but there is also a floating slider for human control.

--- client/Chordify/chordify.js
+++ client/Chordify/chordify.js
@@ -2,6 +2,10 @@ var sp = getSpotifyApi(1);
 var models = sp.require('sp://import/scripts/api/models');
 var player = models.player;
 
+
+var scrollSpeed = 0;
+var scrollInterval = 0;
+
 exports.init = init;
 
 function init() {
@@ -28,6 +32,7 @@ function updatePageWithTrackDetails() {
 	if (playerTrackInfo == null) {
 		header.innerText = "Nothing playing!";
 	} else {
+  
 		var track = playerTrackInfo.data;
 		header.innerHTML = track.name + "/" + track.album.artist.name;
 		cont.innerHTML = 'loading Chords..'
@@ -40,7 +45,13 @@ function updatePageWithTrackDetails() {
 	   			console.log(req.status)
 	    		if (req.status == 200) {
 	    			console.log('200')
-	       			cont.innerHTML = req.responseText
+            cont.innerHTML = req.responseText.substring(req.responseText.search('find the application.') + 21)
+            //Set scroll speed
+            scrollSpeed  = player.track.duration / ($(document).height() - 762);
+            $( "#slider-vertical" ).slider("value", scrollSpeed);
+            $( "#amount" ).val( scrollSpeed );
+            
+            pageScroll(scrollSpeed);
 	     		}
 	   		}
 	  	};
@@ -48,3 +59,24 @@ function updatePageWithTrackDetails() {
 		req.send();
 	}
 }
+
+function pageScroll(speed) {
+  window.clearInterval(scrollInterval);
+  scrollInterval = setInterval(function(){window.scrollBy(0,1)} , speed);
+}
+
+$(function() {
+  $( "#slider-vertical" ).slider({
+    orientation: "vertical",
+    range: "min",
+    min: 0,
+    max: 200,
+    value: scrollSpeed,  
+    slide: function( event, ui ) {
+      $( "#amount" ).val( ui.value );
+      pageScroll(ui.value);
+    }
+  });
+  $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+});
+
